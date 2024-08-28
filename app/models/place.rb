efad265fbbedf4b89e5f7851d7fe7c85627f4a4a
Waishnav <waishnavdeore@gmail.com>
2024-08-28 09:25:50 +0530

    feat: added seed data of mumbai using mapbox api

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,4 @@ class Place < ApplicationRecord
   has_many :activities
   has_many :place_best_times
   has_many :point_of_interests
-  has_many :categories, through: :point_of_interests
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,69 +3,176 @@
 # Places category enum : { hotel: 0, restaurant: 1, attraction: 2, landmark: 3, park: 4, museum: 5, other: 6 }
 
 # Mumbai
-Destination.create(name: "Mumbai", country: "India", latitude: 19.076090, longitude: 72.877426, avg_transportation_cost_per_km: 20.0, avg_food_cost_per_meal: 200.0)
+#Destination.create(name: "Mumbai", country: "India", latitude: 19.076090, longitude: 72.877426, avg_transportation_cost_per_km: 20.0, avg_food_cost_per_meal: 200.0)
+#
+## Places in Mumbai
+#Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Gateway of India", category: :landmark, average_time_spent: 2, opening_hours: "09:00:00", closing_hours: "21:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9220, longitude: 72.8347)
+#Activity.create(place: Place.find_by(name: "Gateway of India"), name: "Boat Ride", category: :adventure, cost: 0.0, average_time_spent: 0.5)
+#Activity.create(place: Place.find_by(name: "Gateway of India"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
+#Activity.create(place: Place.find_by(name: "Gateway of India"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
+#Activity.create(place: Place.find_by(name: "Gateway of India"), name: "Food", category: :fun, cost: 0.0, average_time_spent: 0.5)
+#
+#Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Marine Drive", category: :attraction, average_time_spent: 2, opening_hours: "00:00:00", closing_hours: "23:59:59", min_cost: 0.0, max_cost: 0.0, latitude: 18.9402, longitude: 72.8213)
+#Activity.create(place: Place.find_by(name: "Marine Drive"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
+#Activity.create(place: Place.find_by(name: "Marine Drive"), name: "Food", category: :fun, cost: 0.0, average_time_spent: 0.5)
+#Activity.create(place: Place.find_by(name: "Marine Drive"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
+#Activity.create(place: Place.find_by(name: "Marine Drive"), name: "Relaxation", category: :fun, cost: 0.0, average_time_spent: 0.5)
 
-# Places in Mumbai
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Gateway of India", category: :landmark, average_time_spent: 2, opening_hours: "09:00:00", closing_hours: "21:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9220, longitude: 72.8347)
-Activity.create(place: Place.find_by(name: "Gateway of India"), name: "Boat Ride", category: :adventure, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Gateway of India"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Gateway of India"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Gateway of India"), name: "Food", category: :fun, cost: 0.0, average_time_spent: 0.5)
+india = Country.find_or_create_by(name: "India", code: "in")
+puts india.inspect
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Marine Drive", category: :attraction, average_time_spent: 2, opening_hours: "00:00:00", closing_hours: "23:59:59", min_cost: 0.0, max_cost: 0.0, latitude: 18.9402, longitude: 72.8213)
-Activity.create(place: Place.find_by(name: "Marine Drive"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Marine Drive"), name: "Food", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Marine Drive"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Marine Drive"), name: "Relaxation", category: :fun, cost: 0.0, average_time_spent: 0.5)
+require 'net/http'
+require 'json'
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Juhu Beach", category: :attraction, average_time_spent: 1, opening_hours: "00:00:00", closing_hours: "23:59:59", min_cost: 0.0, max_cost: 0.0, latitude: 19.0974, longitude: 72.8261)
-Activity.create(place: Place.find_by(name: "Juhu Beach"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Juhu Beach"), name: "Food", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Juhu Beach"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
+MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidmFpc2huYXY3NiIsImEiOiJjbTBjanVpdzkwM3p6Mmlxc25ha3lyZ21mIn0.vWhWfnAEsvZ2MUMRgo0u0w'
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Siddhivinayak Temple", category: :landmark, average_time_spent: 1, opening_hours: "05:30:00", closing_hours: "21:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 19.0176, longitude: 72.8309)
-Activity.create(place: Place.find_by(name: "Siddhivinayak Temple"), name: "Prayer", category: :cultural, cost: 0.0, average_time_spent: 0.5)
+def fetch_destination_details(destination_name, country)
+  encoded_place_name = URI.encode_www_form_component("#{destination_name}, #{country}")
+  url = URI("https://api.mapbox.com/geocoding/v5/mapbox.places/#{encoded_place_name}.json?access_token=#{MAPBOX_ACCESS_TOKEN}&types=place")
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Haji Ali Dargah", category: :landmark, average_time_spent: 0.5, opening_hours: "05:30:00", closing_hours: "22:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9829, longitude: 72.8106)
-Activity.create(place: Place.find_by(name: "Haji Ali Dargah"), name: "Prayer", category: :cultural, cost: 0.0, average_time_spent: 0.5)
+  response = Net::HTTP.get_response(url)
+  data = JSON.parse(response.body)
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Chhatrapati Shivaji Maharaj Terminus", category: :landmark, average_time_spent: 0.5, opening_hours: "00:00:00", closing_hours: "23:59:59", min_cost: 0.0, max_cost: 0.0, latitude: 18.9402, longitude: 72.8359)
-Activity.create(place: Place.find_by(name: "Chhatrapati Shivaji Maharaj Terminus"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Chhatrapati Shivaji Maharaj Terminus"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
+  if data['features'].empty?
+    puts "No results found for #{destination_name}"
+    return nil
+  end
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Elephanta Caves", category: :attraction, average_time_spent: 1.5, opening_hours: "09:00:00", closing_hours: "17:30:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9647, longitude: 72.9314)
-Activity.create(place: Place.find_by(name: "Elephanta Caves"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Elephanta Caves"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Elephanta Caves"), name: "Exploration", category: :adventure, cost: 0.0, average_time_spent: 0.5)
+  feature = data['features'][0]
+  {
+    name: feature['text'],
+    longitude: feature['center'][0],
+    latitude: feature['center'][1],
+    country: feature['context'].find { |c| c['id'].start_with?('country') }&.dig('text')
+  }
+end
+ 
+def determine_best_time(category)
+  case category.to_sym
+  when :beach, :lake
+    [:morning, :evening]
+  when :shopping_mall
+    [:afternoon, :evening]
+  when :art_gallery, :museum
+    [:afternoon]
+  when :landscaping, :park, :nature_reserve
+    [:morning, :afternoon]
+  when :nightlife, :nightclub
+    [:night]
+  when :entertainment
+    [:afternoon, :evening, :night]
+  when :tourist_attraction, :historic_site, :monument
+    [:morning, :afternoon]
+  when :temple
+    [:morning, :evening]
+  when :art
+    [:morning, :afternoon, :evening]
+  when :viewpoint
+    [:morning, :evening]
+  when :animal_shelter, :zoo, :aquarium
+    [:morning, :afternoon]
+  when :water_park
+    [:afternoon]
+  when :climbing, :cave
+    [:morning, :afternoon]
+  when :waterfall
+    [:morning, :afternoon]
+  else
+    [:morning, :afternoon, :evening]
+  end
+end
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Chor Bazaar", category: :attraction, average_time_spent: 1, opening_hours: "11:00:00", closing_hours: "19:30:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9668, longitude: 72.8258)
-Activity.create(place: Place.find_by(name: "Chor Bazaar"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
+def fetch_places_details(destination, country, poi)
+  encoded_place_name = URI.encode_www_form_component("#{poi} in #{destination.name}")
+  url = URI("https://api.mapbox.com/geocoding/v5/mapbox.places/#{encoded_place_name}.json?country=#{country.code}&access_token=#{MAPBOX_ACCESS_TOKEN}&types=poi&limit=10&proximity=#{destination.longitude},#{destination.latitude}")
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Sanjay Gandhi National Park", category: :park, average_time_spent: 1, opening_hours: "07:30:00", closing_hours: "18:30:00", min_cost: 0.0, max_cost: 0.0, latitude: 19.2148, longitude: 72.9106)
-Activity.create(place: Place.find_by(name: "Sanjay Gandhi National Park"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Sanjay Gandhi National Park"), name: "Exploration", category: :adventure, cost: 0.0, average_time_spent: 0.5)
+  response = Net::HTTP.get_response(url)
+  data = JSON.parse(response.body)
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Worli Sea Face", category: :attraction, average_time_spent: 1, opening_hours: "00:00:00", closing_hours: "23:59:59", min_cost: 0.0, max_cost: 0.0, latitude: 19.0190, longitude: 72.8154)
-Activity.create(place: Place.find_by(name: "Worli Sea Face"), name: "Photography", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Worli Sea Face"), name: "Relaxation", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Worli Sea Face"), name: "Food", category: :fun, cost: 0.0, average_time_spent: 0.5)
+  if data['features'].empty?
+    puts "No results found for #{poi} in #{destination.name}"
+    return []
+  end
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Global Vipassana Pagoda", category: :landmark, average_time_spent: 0.5, opening_hours: "09:00:00", closing_hours: "19:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 19.2315, longitude: 72.8100)
-Activity.create(place: Place.find_by(name: "Global Vipassana Pagoda"), name: "Meditation", category: :relaxation, cost: 0.0, average_time_spent: 0.5)
+  data['features'].map do |feature|
+    place = Place.find_or_create_by(
+      destination: destination,
+      name: feature['text'],
+      longitude: feature['center'][0],
+      latitude: feature['center'][1],
+    )
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Nehru Science Centre", category: :museum, average_time_spent: 1, opening_hours: "10:00:00", closing_hours: "18:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9940, longitude: 72.8225)
-Activity.create(place: Place.find_by(name: "Nehru Science Centre"), name: "Exploration", category: :adventure, cost: 0.0, average_time_spent: 0.5)
+    # renamed categories in our db
+    category_mapping = {
+      'tourism' => 'tourist_attraction',
+      'attraction' => 'tourist_attraction',
+    }
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Colaba Causeway", category: :attraction, average_time_spent: 1, opening_hours: "10:00:00", closing_hours: "21:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9166, longitude: 72.8323)
-Activity.create(place: Place.find_by(name: "Colaba Causeway"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
+    # saving each category in PointOfInterest table
+    mapbox_categories = feature['properties']['category']&.split(', ') || []
+    mapbox_categories.each do |category|
+      category_downcase = category.downcase
+      mapped_category = category_mapping[category_downcase] || category_downcase
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Crawford Market", category: :attraction, average_time_spent: 1, opening_hours: "10:00:00", closing_hours: "20:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9503, longitude: 72.8332)
-Activity.create(place: Place.find_by(name: "Crawford Market"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
+      if PointOfInterest.categories.key?(mapped_category.to_sym)
+        PointOfInterest.find_or_create_by(
+          place: place,
+          category: mapped_category
+        )
+      end
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "Phoenix Market City", category: :attraction, average_time_spent: 1, opening_hours: "11:00:00", closing_hours: "22:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 19.0872, longitude: 72.8792)
-Activity.create(place: Place.find_by(name: "Phoenix Market City"), name: "Shopping", category: :fun, cost: 0.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "Phoenix Market City"), name: "Food", category: :fun, cost: 0.0, average_time_spent: 0.5)
+      best_times = determine_best_time(mapped_category)
 
-Place.create(destination: Destination.find_by(name: "Mumbai"), name: "High Street Phoenix", category: :attraction, average_time_spent: 1, opening_hours: "11:00:00", closing_hours: "22:00:00", min_cost: 0.0, max_cost: 0.0, latitude: 18.9937, longitude: 72.8208)
-Activity.create(place: Place.find_by(name: "High Street Phoenix"), name: "Shopping", category: :fun, cost: 200.0, average_time_spent: 0.5)
-Activity.create(place: Place.find_by(name: "High Street Phoenix"), name: "Food", category: :fun, cost: 300.0, average_time_spent: 0.5)
+      best_times.each do |best_time|
+        PlaceBestTime.find_or_create_by(
+          place: place,
+          best_time_to_visit: best_time
+        )
+      end
+    end
+
+    place
+  end
+end
+
+# Destinations
+destinations = [
+  { name: "Mumbai", country: "India" },
+  #{ name: "New Delhi", country: "India" },
+  #{ name: "Nashik", country: "India" }, 
+  #{ name: "Pune", country: "India" },
+  #{ name: "Nagpur", country: "India" },
+  #{ name: "Goa", country: "India" },
+  #{ name: "Jaipur", country: "India" },
+  #{ name: "Agra", country: "India" },
+  #{ name: "Kolkata", country: "India" },
+  #{ name: "Chennai", country: "India" },
+  #{ name: "Bengaluru", country: "India" },
+  #{ name: "Hyderabad", country: "India" },
+]
+categories = PointOfInterest.categories.keys.map(&:to_s)
+
+destinations.each do |d|
+  details = fetch_destination_details(d[:name], d[:country])
+  next if details.nil?
+
+  dest = Destination.find_or_create_by(
+    name: d[:name],
+    country: india,
+    latitude: details[:latitude],
+    longitude: details[:longitude],
+    avg_transportation_cost_per_km: 20.0,
+    avg_food_cost_per_meal: 200.0
+  )
+
+  puts "Created destination: #{dest.name}"
+
+  # array of categories or point of interests
+  # iteration over these categories and fetching place details and storing it the places in our db
+  categories.each do |category|
+    places = fetch_places_details(dest, india, category)
+    puts "  Found #{places.length} places for category: #{category}"
+  end
+end
+
+puts "Destination and place creation completed."
