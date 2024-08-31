ff44af2968beff1db4b3b783504096ef416fc563
Waishnav <waishnavdeore@gmail.com>
2024-09-01 02:26:31 +0530

    adding domain allow list

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.log_level = :info
+  config.hosts << "nalta.live"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
