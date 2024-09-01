23439346cf67e8747bdd04bb39de42917a97d561
Waishnav <waishnavdeore@gmail.com>
2024-09-01 05:47:08 +0530

    fix no two duplicate places with same name

--- db/migrate/20240901000948_add_indexes_to_tables.rb
+++ db/migrate/20240901000948_add_indexes_to_tables.rb
@@ -6,7 +6,7 @@ class AddIndexesToTables < ActiveRecord::Migration[7.1]
     add_index :destinations, [:latitude, :longitude] unless index_exists?(:destinations, [:latitude, :longitude])
 
     # Add indexes to places table
-    add_index :places, :name unless index_exists?(:places, :name)
+    add_index :places, :name, unique: true unless index_exists?(:places, :name, unique: true)
 
     # Add index to point_of_interests table
     add_index :point_of_interests, :category unless index_exists?(:point_of_interests, :category)
